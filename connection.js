4d372333297391f44a4eb94d1c9539820d9ab559
Arthur Simonsson <arturo@MacBook-Pro.lan>
2020-04-08 15:58:52 +0200

    tcp response working against chip

--- connection.js
+++ connection.js
@@ -6,18 +6,23 @@ exports.onClientConnected = function(sock) {
    
     // sock.on('connect', async function(data) {
     //     console.log('%s Says: %s', remoteAddress, data);
-    //     return await model.getWelcome().then(data => sock.write(data.message));
     // });
-    // var message = await 
-    model.getWelcome().then(data => sock.write(data.message));
+
+    sock.write('connection initiated\n');
     
     sock.on('data', async function(data) {
         console.log('%s Says: %s', remoteAddress, data);
-        model.getWelcome().then(data => sock.write(data.message));
+        var value = data.toString().replace(/(\r\n|\n|\r)/gm,"");
+        if (value.localeCompare('welcome') === 0) {
+          // sock.write('kth open')
+          model.getWelcome().then(data => sock.write(data.message));
+        }
     });
+
     sock.on('close',  function () {
       console.log('connection from %s closed', remoteAddress);
     });
+
     sock.on('error', function (err) {
       console.log('Connection %s error: %s', remoteAddress, err.message);
     });
