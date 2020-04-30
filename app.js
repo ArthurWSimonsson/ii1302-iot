56d1c56c464d1393fef14687ddc1257c98f03752
Arthur Simonsson <arturo@MacBook-Pro.lan>
2020-04-30 15:15:28 +0200

    test

--- app.js
+++ app.js
@@ -6,17 +6,18 @@ var appEnv = cfenv.getAppEnv();
 
 
 // const HOST = 'localhost';
-const HOST = '192.168.10.231';
+const HOST = '35.230.145.234';
+// const HOST = '192.168.10.231';
 const PORT = 3000;
 
 // port forward : 213.114.145.242 portnummer 3001
 
 var server = net.createServer(con.onClientConnected);  
  
-// server.listen(PORT, HOST, function() {  
-//   console.log('server listening on %j', server.address());
-// });
-
-server.listen(appEnv.port || 8080, '0.0.0.0', function() {  
+server.listen(PORT, HOST, function() {  
   console.log('server listening on %j', server.address());
 });
+
+// server.listen(appEnv.port || 8080, '0.0.0.0', function() {  
+//   console.log('server listening on %j', server.address());
+// });
