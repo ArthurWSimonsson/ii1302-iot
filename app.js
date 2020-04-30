c48451cd7ba266e9601c00e105910bd4e384f525
Arthur Simonsson <arturo@MacBook-Pro.lan>
2020-04-30 22:05:16 +0200

    test

--- app.js
+++ app.js
@@ -43,6 +43,6 @@ app.get('/', function (req, res) {
    res.send('Hello World from GCE!'); 
 });
 
-app.listen(3000, function () {
+app.listen(63000, function () {
    console.log('Example app listening on port 3000!');
 });
